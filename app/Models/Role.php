2a4459b9866845228de3f25ff808bd95421ac7c7
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-04-27 14:29:53 +0100

    roles relationship

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Role extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public const Is_Patient = 1; 
+    public const Is_Staff = 2; 
+    public const Is_Admin = 3; 
+
+    public function user(): BelongsTo {
+
+        return $this->belongsTo(User::class);
+    }
 }
